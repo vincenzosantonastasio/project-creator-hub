18082b35e271ea5fff2cb883501b511608e50cbf
vincenzosantonastasio <vincenzosantonastasio@gmail.com>
2026-03-18 23:27:39 +0100

    Create creators.js

---
+++ backend/creators.js
@@ -0,0 +1,14 @@
+const creators = [
+  {
+    id: 1,
+    name: "Sample Creator",
+    platforms: ["YouTube", "TikTok"]
+  },
+  {
+    id: 2,
+    name: "Another Creator",
+    platforms: ["Instagram"]
+  }
+];
+
+module.exports = creators;
