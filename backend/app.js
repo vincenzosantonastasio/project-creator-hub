2e307f1dadee92c2fa618d0bb37e960e3df7112d
vincenzosantonastasio <vincenzosantonastasio@gmail.com>
2026-03-19 08:27:05 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,33 @@ const http = require("http");
 const creators = require("./creators");
 
 const server = http.createServer((req, res) => {
+
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Creator Hub API running" }));
+
   } else if (req.url === "/creator") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-    JSON.stringify(creators)  
-    );
+    res.end(JSON.stringify(creators));
+
+  } else if (req.url.startsWith("/creator/")) {
+    const id = parseInt(req.url.split("/")[2]);
+
+    const creator = creators.find(c => c.id === id);
+
+    if (creator) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(creator));
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+
   } else {
     res.writeHead(404);
     res.end();
   }
+
 });
 
 server.listen(3000, () => {
