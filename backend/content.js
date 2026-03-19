3536464d9a4862669674005e534f423d02d71ee5
vincenzosantonastasio <vincenzosantonastasio@gmail.com>
2026-03-19 08:34:43 +0100

    Create content.js

---
+++ backend/content.js
@@ -0,0 +1,25 @@
+const content = [
+  {
+    id: 1,
+    creator_id: 1,
+    title: "I Built 100 Wells in Africa",
+    platform: "YouTube",
+    url: "https://youtube.com"
+  },
+  {
+    id: 2,
+    creator_id: 2,
+    title: "Khaby Reaction Video",
+    platform: "TikTok",
+    url: "https://tiktok.com"
+  },
+  {
+    id: 3,
+    creator_id: 3,
+    title: "Logan Paul Podcast",
+    platform: "YouTube",
+    url: "https://youtube.com"
+  }
+];
+
+module.exports = content;
