1a889a853dc844ac233edfdfcc5cf8a654bcef29
vincenzosantonastasio <vincenzosantonastasio@gmail.com>
2026-03-19 14:25:23 +0100

    Add creator avatars

--- backend/creators.js
+++ backend/creators.js
@@ -2,16 +2,19 @@ const creators = [
   {
     id: 1,
     name: "MrBeast",
+    avatar: "https://i.pravatar.cc/150?img=1",
     platforms: ["YouTube"]
   },
   {
     id: 2,
     name: "Khaby Lame",
+    avatar: "https://i.pravatar.cc/150?img=2",
     platforms: ["TikTok", "Instagram"]
   },
   {
     id: 3,
     name: "Logan Paul",
+    avatar: "https://i.pravatar.cc/150?img=3",
     platforms: ["YouTube", "Instagram"]
   }
 ];
